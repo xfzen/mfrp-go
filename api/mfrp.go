9a55dab89d9a699a513d33f37c002506444a417f
Essen <hoksum.guo@gmail.com>
2024-09-13 15:57:39 +0800

    fixed signal killed issue

--- api/mfrp.go
+++ api/mfrp.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
 
 	"mfrp/api/internal/handler"
 	"mfrp/api/internal/svc"
@@ -16,6 +20,18 @@ var configFile = flag.String("f", "etc/mfrp-api.yaml", "the config file")
 
 func main() {
 	flag.Parse()
+
+	runtime.GOMAXPROCS(1)              // 限制 CPU 使用数，避免过载
+	runtime.SetMutexProfileFraction(1) // 开启对锁调用的跟踪
+	runtime.SetBlockProfileRate(1)     // 开启对阻塞操作的跟踪
+
+	go func() {
+		// 启动一个 http server，注意 pprof 相关的 handler 已经自动注册过了
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
 
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
